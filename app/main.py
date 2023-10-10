b95ba4db4619aa5276badfc5c01729e095898051
Kirill <kirill@startted.com>
2023-10-10 10:29:10 +0300

    added new config init

--- app/main.py
+++ app/main.py
@@ -8,5 +8,6 @@ from app.utils.constants.config import CONFIG_LOCAL_PATH
 config_path = os.environ.get("CONFIG", CONFIG_LOCAL_PATH)
 config = ConfigFactory.get_from_path(config_path=config_path)
 
+# init app
 app = AppBuilder().with_config(config).build()
 app.full_init()
